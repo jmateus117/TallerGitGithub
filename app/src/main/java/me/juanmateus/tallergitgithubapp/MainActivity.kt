05d0bef646dddbbda5e1a7d7b02b022ab5e2e27f
Juan Mateus <jmateus410@unab.edu.co>
2026-09-16 10:10:28 -0500

    feat: agrega imagen personal

--- app/src/main/java/me/juanmateus/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/juanmateus/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.juanmateus.tallergitgithubapp.ui.theme.TallerGitGitHubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +28,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+        }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GitGithub(){
+    Card() {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier =Modifier.fillMaxSize()) {
+            val painter = painterResource(id = R.drawable.fotodeperfil)
+            val contentDescription = "Mi foto de perfil"
+            Image(painter = painter,
+                contentDescription = contentDescription, modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape))
+
         }
     }
 }
